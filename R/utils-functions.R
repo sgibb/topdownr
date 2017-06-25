2f22842f7cf24e58c3d40490bc42b94e3ccbe1c7
Sebastian Gibb <mail@sebastiangibb.de>
2017-06-25 14:56:26 +0200

    add .targetMassList2Mz

--- R/utils-functions.R
+++ R/utils-functions.R
@@ -21,3 +21,14 @@ cat0 <- function(...) {
   stopifnot(is.list(x))
   vapply(x, nrow, double(1L))
 }
+
+#' The ScanHeadsMan output for the scan conditons contains a column
+#' TargetedMassList with the format "(mz=[0-9]+\.[0-9]+ z=[0-9]+ name=)". This
+#' function converts this format to truncated (one decimal place) mz values.
+#' @param x character
+#' @return double
+#' @noRd
+.targetMassList2Mz <- function(x) {
+  stopifnot(is.character(x))
+  trunc(as.double(gsub("^.*mz=([^ ]+) z.*$", "\\1", x)) * 10L) / 10L
+}
--- tests/testthat/test_utils-functions.R
+++ tests/testthat/test_utils-functions.R
@@ -15,3 +15,10 @@ test_that(".nrows", {
   expect_equal(topdown:::.nrows(list(matrix(nrow=2, ncol=2),
                                      matrix(nrow=3, ncol=2))), 2:3)
 })
+
+test_that(".targetMassList2Mz", {
+  expect_error(topdown:::.targetMassList2Mz(1:3))
+  expect_equal(topdown:::.targetMassList2Mz(c("(mz=1000.12 z=2 name=foo)",
+                                              "(mz=933.99 z=3 name=)")),
+               c(1000.1, 933.9))
+})
