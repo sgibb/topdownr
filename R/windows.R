4edbdc56ea4f2ce53e9858a516c948c9b0a3e8c0
Sebastian Gibb <mail@sebastiangibb.de>
2017-07-03 20:45:47 +0200

    add runScanHeadsman

--- R/windows.R
+++ R/windows.R
@@ -11,6 +11,9 @@
 #' @param verbose \code{logical}, if \code{TRUE} a progress bar is shown.
 #' @return Nothing. Used for its side effects.
 #' @seealso \code{\link{writeMethodXmls}}
+#' @references XmlMethodChanger source code:
+#' \url{https://github.com/thermofisherlsms/meth-modifications}
+#' @rdname XmlMethodChanger
 #' @export
 createTngFusionMethFiles <- function(template,
                                      xml=list.files(pattern=".*\\.xml$"),
@@ -43,9 +46,44 @@ createTngFusionMethFiles <- function(template,
   }
 }
 
+#' @rdname XmlMethodChanger
+#' @export
+runXmlMethodChanger <- createTngFusionMethFiles
+
 .xmlMethodChanger <- function(exe, input, output, modification) {
   args <- paste0("--", c("input", "output", "modification"), "=",
                  shQuote(c(input, output, modification)))
+  system2(normalizePath(exe), args=args)
+}
+
+#' Create .csv/.txt files from raw files
+#'
+#' This function calls \sQuote{ScanHeadsman.exe} on a given directory containing
+#' RAW files.
+#' It works only on Windows.
+#'
+#' @param executable \code{character}, path to the
+#'  \sQuote{ScanHeadsman.exe} executable.
+#' @param path \code{character}, path to the directory containing the raw files.
+#' @return Nothing. Used for its side effects.
+#' @seealso \code{\link{XmlMethodChanger}}
+#' @rdname ScanHeadsman
+#' @references ScanHeadsman source code:
+#' \url{https://bitbucket.org/caetera/scanheadsman}
+#' @export
+runScanHeadsman <- function(executable="ScanHeadsman.exe",
+                            path=".") {
+  if (.Platform$OS.type != "windows") {
+    stop("This function works only on Windows.")
+  }
+  if (!file.exists(executable)) {
+    stop(sQuote(executable), " not found!")
+  }
+  .scanHeadsman(exe=executable, path=path)
+}
+
+.scanHeadsman <- function(exe, path=".") {
+  args <- c(paste0("--", c("noMS", "methods:CSV")), path)
   system2(normalizePath(exe), args=args)
 }
 
