0e9503bfab060e0f79facfeadccd1d7c7f049cf6
Sebastian Gibb <mail@sebastiangibb.de>
2017-07-18 12:55:14 +0200

    add show method for FragmentViews

---
+++ R/methods-FragmentViews.R
@@ -0,0 +1,41 @@
+#' @param object FragmentViews
+#' @noRd
+setMethod("show", "FragmentViews", function(object) {
+  cat0(class(object), " on a ", length(subject(object)), "-letter sequence:\n")
+  cat0("  ", .snippet(as.character(subject(object)),
+                      getOption("width") - 2L), "\n")
+  cat0("Views:\n")
+
+  entries <- list(
+    ## rownames
+    c("", .hft(paste0("[", seq_len(length(object)), "]"), n=5L)),
+    ## ranges
+    start=c("start", .hft(start(object), n=5L)),
+    end=c("end", .hft(end(object), n=5L)),
+    width=c("width", .hft(width(object), n=5L)),
+    ## metadata
+    mass=c("mass", .hft(sprintf("%.2f", elementMetadata(object)$mass), n=5L)),
+    type=c("type", .hft(as.character(elementMetadata(object)$type), n=5L)),
+    z=c("z", as.character(.hft(elementMetadata(object)$z, n=5L)))
+  )
+
+  cw <- .vapply1d(entries, function(e)max(nchar(e)))
+
+  ## views
+  mw <- sum(cw + 1L)
+  ## n == 5 + 1 (to compensate for the fill ("...") below
+  vw <- as(object[.hft(seq_len(length(object)), fill=NULL, n=6L)], "character")
+  vw <- c("",
+          .hft(.snippet(paste0("[", vw, "]"), getOption("width") - mw), n=5L))
+
+
+  ## format
+  just <- c(rep("right", 5), "left", "right", "left")
+  entries <- mapply(format, x=c(entries, list(vw)), justify=just)
+
+  ## output
+  out <- paste(apply(entries, 1L, paste, collapse=" "), collapse="\n")
+  cat0(out, "\n")
+
+  invisible(NULL)
+})
--- tests/testthat/test_FragmentViews.R
+++ tests/testthat/test_FragmentViews.R
@@ -1,17 +1,27 @@
 context("FragmentViews")
 
-test_that("constructor", {
-  fv <- FragmentViews("ACE", start=3:1, end=3, names=paste0("x", 1:3),
-                      mass=c(174.039696, 334.070346, 405.107456),
-                      type=rep("x", 3), z=rep(1, 3))
-  rv <- topdown:::.calculateFragments("ACE", type="x",
-                                      neutralLoss=list(water=NULL))
+fv <- FragmentViews("ACE", start=3:1, end=3, names=paste0("x", 1:3),
+                    mass=c(174.039696, 334.070346, 405.107456),
+                    type=rep("x", 3), z=rep(1, 3))
 
+test_that("constructor", {
+  expect_equal(topdown:::.calculateFragments("ACE", type="x",
+                                             neutralLoss=list(water=NULL)), fv)
+})
+
+test_that("show", {
+  expect_output(show(fv),
+                paste(c("FragmentViews on a 3-letter sequence:",
+                        "  ACE",
+                        "Views:",
+                        "    start end width   mass type z *",
+                        "\\[1\\]     3   3     1 174\\.04 x    1 \\[E\\] *",
+                        "\\[2\\]     2   3     2 334\\.07 x    1 \\[CE\\] *",
+                        "\\[3\\]     1   3     3 405\\.11 x    1 \\[ACE\\]"),
+                      collapse="\n"))
 })
 
 test_that("validity", {
-  fv <- FragmentViews("ACE", start=3:1, end=3, names=paste0("x", 1:3),
-                      mass=as.double(1:3), type=rep("x", 3), z=rep(1, 3))
   expect_true(validObject(fv))
   d <- elementMetadata(fv)
   elementMetadata(fv) <- NULL
