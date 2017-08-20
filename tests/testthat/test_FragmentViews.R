4390fb578c93d93856754f0a853704afe46a7cc5
Sebastian Gibb <mail@sebastiangibb.de>
2017-08-20 15:59:33 +0200

    fix FragmentViews unit tests according to lgatto/MSnbase#248

--- tests/testthat/test_FragmentViews.R
+++ tests/testthat/test_FragmentViews.R
@@ -1,78 +1,56 @@
 context("FragmentViews")
 
-fv <- FragmentViews("ACE", start=3:1, end=3, names=paste0("x", 1:3),
-                    mass=c(174.039696, 334.070346, 405.107456),
-                    type=rep("x", 3), z=rep(1, 3),
+fv <- FragmentViews("AACE", start=1, end=1:3, names=paste0("b", 1:3),
+                    mass=c(114.054951, 185.092061, 345.122711),
+                    type=rep("b", 3), z=rep(1, 3),
                     metadata=list(modifications=c("Carbamidomethyl",
                                                   "Met-loss+Acetyl")))
 
 test_that("constructor", {
-    expect_error(topdown:::.calculateFragments("ACE", modifications="FOO"))
-    expect_equal(topdown:::.calculateFragments("ACE", type="x",
-                                               neutralLoss=list(water=NULL)),
-                 fv)
-    fv1 <- FragmentViews("AC", start=1:2, end=2, names=paste0("x", 2:1),
-                         mass=c(276.064870 + 42.010565, 205.027760),
-                         type=rep("x", 2), z=rep(1, 2),
-                         metadata=list(modifications=c("Carbamidomethyl",
-                                                       "Met-loss+Acetyl")))
-    expect_equal(topdown:::.calculateFragments("MAC", type="x",
-                                               neutralLoss=list(water=NULL)),
-                 fv1, tolerance=1e-7)
-    fv2 <- FragmentViews("AC", start=2:1, end=2, names=paste0("x", 1:2),
-                         mass=c(148.006296, 219.043406 + 42.010565),
-                         type=rep("x", 2), z=rep(1, 2),
-                         metadata=list(modifications="Acetyl"))
-    expect_equal(topdown:::.calculateFragments("AC", type="x",
-                                               modifications="Acetyl",
-                                               neutralLoss=list(water=NULL)),
-                 fv2, tolerance=1e-7)
-    fv3 <- FragmentViews("AC", start=1:2, end=2, names=paste0("x", 2:1),
-                         mass=c(276.064870, 205.027760) - 57.021464,
-                         type=rep("x", 2), z=rep(1, 2),
-                         metadata=list(modifications="Met-loss"))
-    expect_equal(topdown:::.calculateFragments("MAC", type="x",
-                                               modifications="Met-loss",
-                                               neutralLoss=list(water=NULL)),
-                 fv3, tolerance=1e-7)
-    fv4 <- FragmentViews("AC", start=1:2, end=2, names=paste0("x", 2:1),
-                         mass=c(276.064870 + 42.010565, 205.027760) - 57.021464,
-                         type=rep("x", 2), z=rep(1, 2),
-                         metadata=list(modifications="Met-loss+Acetyl"))
-    expect_equal(topdown:::.calculateFragments("MAC", type="x",
-                                               modifications="Met-loss+Acetyl",
-                                               neutralLoss=list(water=NULL)),
-                 fv4, tolerance=1e-7)
-    fv5 <- FragmentViews("CA", start=2:1, end=2, names=paste0("x", 1:2),
-                         mass=c(116.034216, 276.064866 - 57.021464),
-                         type=rep("x", 2), z=rep(1, 2),
-                         metadata=list(modifications="Met-loss+Acetyl"))
-    expect_equal(topdown:::.calculateFragments("MCA", type="x",
-                                               modifications="Met-loss+Acetyl",
-                                               neutralLoss=list(water=NULL)),
-                 fv5, tolerance=1e-7)
-    fv6 <- FragmentViews("CA", start=2:1, end=2, names=paste0("x", 1:2),
-                         mass=c(116.034216, 219.043406),
-                         type=rep("x", 2), z=rep(1, 2),
-                         metadata=list(modifications=NULL))
-    expect_equal(topdown:::.calculateFragments("CA", type="x",
-                                               modifications=NULL,
-                                               neutralLoss=list(water=NULL)),
-                 fv6, tolerance=1e-7)
+    expect_error(topdown:::.calculateFragments("AACE", modifications="FOO"))
+    expect_equal(topdown:::.calculateFragments("MAACE", type="b"), fv)
+
+    ## no Acetylation because no ^M
+    fv1 <- fv
+    fv1@elementMetadata$mass <- fv1@elementMetadata$mass - 42.010565
+    expect_equal(topdown:::.calculateFragments("AACE", type="b"),
+                 fv1)
+    ## Acetylation but no Carboxyamidomethylation
+    fv2 <- fv
+    fv2@elementMetadata$mass[3] <- fv@elementMetadata$mass[3] - 57.021464
+    fv2@metadata$modifications <- c("Acetyl")
+    expect_equal(topdown:::.calculateFragments("AACE", type="b",
+                                               modifications="Acetyl"),
+                 fv2)
+    ## just Met-loss without Acetylation
+    fv3 <- fv
+    fv3@elementMetadata$mass <- fv@elementMetadata$mass - 42.010565
+    fv3@elementMetadata$mass[3] <- fv3@elementMetadata$mass[3] - 57.021464
+    fv3@metadata$modifications <- c("Met-loss")
+    expect_equal(topdown:::.calculateFragments("MAACE", type="b",
+                                               modifications="Met-loss"),
+                 fv3)
+    ## no modification
+    fv4 <- fv
+    fv4@elementMetadata$mass <- fv@elementMetadata$mass - 42.010565
+    fv4@elementMetadata$mass[3] <- fv4@elementMetadata$mass[3] - 57.021464
+    fv4@metadata <- list(modifications=NULL)
+    expect_equal(topdown:::.calculateFragments("AACE", type="b",
+                                               modifications=NULL), fv4)
 })
 
 test_that("show", {
     expect_output(show(fv),
-                  paste(c("FragmentViews on a 3-letter sequence:",
-                          "  ACE",
+                  paste(c("FragmentViews on a 4-letter sequence:",
+                          "  AACE",
                           "Modifications:",
                           "  Carbamidomethyl",
                           "  Met-loss\\+Acetyl",
                           "Views:",
                           "    start end width   mass type z *",
-                          "\\[1\\]     3   3     1 174\\.04 x    1 \\[E\\] *",
-                          "\\[2\\]     2   3     2 334\\.07 x    1 \\[CE\\] *",
-                          "\\[3\\]     1   3     3 405\\.11 x    1 \\[ACE\\]"),
+                          "\\[1\\]     1   1     1 114\\.05 b    1 \\[A\\] *",
+                          "\\[2\\]     1   2     2 185\\.09 b    1 \\[AA\\] *",
+                          "\\[3\\]     1   3     3 345\\.12 b    1 \\[AAC\\]"),
                         collapse="\n"))
 })
 
