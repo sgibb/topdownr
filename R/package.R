5446a4e2c716a27a80ef1ad6bd6be0b0d9c4e4c1
Sebastian Gibb <mail@sebastiangibb.de>
2017-09-22 11:55:12 +0200

    Import quantile from stats

--- R/package.R
+++ R/package.R
@@ -22,7 +22,7 @@
 #'
 #' @importFrom mzR openMSfile close header peaks
 #' @importFrom MSnbase calculateFragments defaultNeutralLoss get.amino.acids
-#' @importFrom stats ave median setNames
+#' @importFrom stats ave median setNames quantile
 #' @importFrom tools file_ext file_path_sans_ext
 #' @importFrom utils modifyList txtProgressBar setTxtProgressBar object.size
 #' read.csv .DollarNames
