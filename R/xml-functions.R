bc43ad5eaab7839cd84b40d792428c9bed2ac0d7
Sebastian Gibb <mail@sebastiangibb.de>
2018-09-23 12:55:49 +0200

    Add .xmlValidMsSettings

--- R/xml-functions.R
+++ R/xml-functions.R
@@ -1,28 +1,78 @@
 #' Find xml children names and tags
 #'
-#' @return `data.frame`, of valid tag names and their types
+#' @param xsd `xml2::xml_document`
+#' @param name `character`, element name
+#'
+#' @return `matrix` of valid tag names and their types
 #' @noRd
 .xmlChildrenNameType <- function(xsd, name) {
+    requireNamespace("xml2")
     nds <- xml2::xml_find_all(
         xsd,
         sprintf(".//xs:element[@name = '%s']//xs:element", name)
     )
     attrs <- xml2::xml_attrs(nds)
     m <- do.call(rbind, lapply(attrs, "[", c("name", "type")))
-    m[, "type"] <- gsub("xs:double", "double", m[, "type"], fixed=TRUE)
-    m[, "type"] <- gsub("xs:int", "integer", m[, "type"], fixed=TRUE)
-    m[, "type"] <- gsub("xs:boolean", "logical", m[, "type"], fixed=TRUE)
+    repl <- matrix(c("double$", "double",
+                     "int.*$", "integer",
+                     "boolean$", "logical",
+                     "string$", "character"), byrow=TRUE, nrow=4)
+    repl[, 1L] <- paste0("^xs:", repl[, 1L])
+    for (i in seq_len(nrow(repl))) {
+        m[, "type"] <- sub(repl[i, 1L], repl[i, 2L], m[, "type"])
+    }
+    colnames(m) <- c("name", "class")
     m
 }
 
 #' Find xml enumerations
 #'
+#' @param xsd `xml2::xml_document`
+#' @param name `character`, element name
+#'
 #' @return `character` of valid enumerations
 #' @noRd
 .xmlEnumeration <- function(xsd, name) {
+    requireNamespace("xml2")
     nds <- xml2::xml_find_all(
         xsd,
         sprintf(".//xs:simpleType[@name = '%s']//xs:enumeration", name)
     )
     xml2::xml_attr(nds, "value")
+}
+
+#' Find TMSnScan parameters
+#'
+#' @param xsd `xml2::xml_document`
+#'
+#' @return `matrix` (`character`)
+#' @noRd
+.xmlTmsnScanParamters <- function(xsd) {
+    param <- .xmlChildrenNameType(xsd, "TMSnScan")
+    isEnum <-
+        !param[, "class"] %in% c("logical", "integer", "double", "character")
+    param[isEnum, "class"] <- vapply(
+        param[isEnum, "class"],
+        function(tt)paste0(.xmlEnumeration(xsd, tt), collapse=":"),
+        NA_character_
+    )
+    # Exclude "Ms2 parameters"
+    param[!startsWith(param[, "name"], "Ms2"),]
+}
+
+#' Find valid MS1/MSn settings
+#'
+#' @param xsd `xml2::xml_document`
+#' @return `matrix` (`character`), 3 columns: name, class, type
+#' @noRd
+.xmlValidMsSettings <- function(xsd) {
+    settings <- rbind(
+        cbind(.xmlChildrenNameType(xsd, "FullMSScan"), type="MS1"),
+        cbind(.xmlTmsnScanParamters(xsd), type="MS2"))
+
+    for (actType in c("ETD", "CID", "HCD", "UVPD")) {
+        isType <- grepl(actType, settings[, "name"])
+        settings[isType, "type"] <- actType
+    }
+    settings
 }
---
+++ tests/testthat/test_xml.R
@@ -0,0 +1,81 @@
+context("xml")
+
+xsd <- '<?xml version="1.0" encoding="utf-8"?>
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" >
+  <xs:simpleType name="Version">
+    <xs:restriction base="xs:string">
+      <xs:enumeration value="1"/>
+      <xs:enumeration value="2"/>
+    </xs:restriction>
+  </xs:simpleType>
+  <xs:element name="FullMSScan">
+    <xs:complexType>
+      <xs:all>
+        <xs:element name="FirstMass" type="xs:double" minOccurs="0" maxOccurs="1" />
+        <xs:element name="LastMass" type="xs:double" minOccurs="0" maxOccurs="1" />
+        <xs:element name="Microscans" type="xs:int" minOccurs="0" maxOccurs="1" />
+      </xs:all>
+    </xs:complexType>
+  </xs:element>
+  <xs:element name="TMSnScan">
+    <xs:complexType>
+      <xs:all>
+        <xs:element name="AgcTarget" type="xs:double" minOccurs="0" maxOccurs="1" />
+        <!-- ETD Params -->
+        <xs:element name="ETDReactionTime" type="xs:double" minOccurs="0" maxOccurs="1" />
+        <xs:element name="CIDCollisionEnergy" type="xs:double" minOccurs="0" maxOccurs="1"/>
+        <xs:element name="CIDActivationQ" type="xs:double" minOccurs="0" maxOccurs="1"/>
+        <xs:element name="Ms2CIDCollisionEnergy" type="xs:double" minOccurs="0" maxOccurs="1"/>
+        <xs:element name="Ms2CIDActivationQ" type="xs:double" minOccurs="0" maxOccurs="1"/>
+      </xs:all>
+    </xs:complexType>
+  </xs:element>
+</xs:schema>'
+
+test_that(".xmlChildrenNameType", {
+    skip_if_not_installed("xml2")
+    expect_equal(
+        topdownr:::.xmlChildrenNameType(xml2::read_xml(xsd), "FullMSScan"),
+        cbind(
+            name=c("FirstMass", "LastMass", "Microscans"),
+            class=c("double", "double", "integer")
+        )
+    )
+})
+
+test_that(".xmlEnumeration", {
+    skip_if_not_installed("xml2")
+    expect_equal(
+        topdownr:::.xmlEnumeration(xml2::read_xml(xsd), "Version"),
+        c("1", "2")
+    )
+})
+
+test_that(".xmlTmsnScanParamters", {
+    skip_if_not_installed("xml2")
+    expect_equal(
+        topdownr:::.xmlTmsnScanParamters(xml2::read_xml(xsd)),
+        cbind(
+            name=c(
+                "AgcTarget", "ETDReactionTime", "CIDCollisionEnergy",
+                "CIDActivationQ"
+            ),
+            class=rep("double", 4)
+        )
+    )
+})
+
+test_that(".xmlValidMsSettings", {
+    skip_if_not_installed("xml2")
+    expect_equal(
+        topdownr:::.xmlValidMsSettings(xml2::read_xml(xsd)),
+        cbind(
+            name=c(
+                "FirstMass", "LastMass", "Microscans", "AgcTarget",
+                "ETDReactionTime", "CIDCollisionEnergy", "CIDActivationQ"
+            ),
+            class=rep(c("double", "integer", "double"), c(2, 1, 4)),
+            type=rep(c("MS1", "MS2", "ETD", "CID"), c(3, 1, 1, 2))
+        )
+    )
+})
